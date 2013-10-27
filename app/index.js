91f32656f9fd940d9ee26baaa7f4b7bc6db534cc
Siedrix <Siedrix@gmail.com>
2013-10-27 01:30:52 -0500

    first commit

---
+++ app/index.js
@@ -0,0 +1,60 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var PaperpressGenerator = module.exports = function PaperpressGenerator(args, options, config) {
+    yeoman.generators.Base.apply(this, arguments);
+
+    this.on('end', function () {
+        this.installDependencies({ skipInstall: options['skip-install'] });
+
+        console.log('Paperpress blog ready');
+    });
+
+    // this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(PaperpressGenerator, yeoman.generators.Base);
+
+PaperpressGenerator.prototype.askFor = function askFor() {
+    var cb = this.async();
+
+    // have Yeoman greet the user.
+    console.log(this.yeoman);
+
+    var prompts = [{
+        name: 'blogName',
+        message: 'What do you want to call your blog?'
+    }];
+
+    this.prompt(prompts, function (props) {
+        this.blogName = props.blogName;
+
+        cb();
+    }.bind(this));
+};
+
+PaperpressGenerator.prototype.app = function app() {
+    this.mkdir('static');
+    this.mkdir('static/articles');
+    this.mkdir('static/layouts');
+    this.mkdir('static/pages');
+    this.mkdir('static/public');
+    this.mkdir('static/public/style');
+
+    this.copy('_package.json', 'package.json');
+    this.copy('_gitignore', '.gitignore');
+    this.copy('_server.js', 'server.js');
+
+    this.copy('layouts/layout.html', 'static/layouts/layout.html');
+    this.copy('layouts/single.html', 'static/layouts/single.html');
+    this.copy('layouts/multiple.html', 'static/layouts/multiple.html');
+
+    this.copy('style/main.css', 'static/public/style/main.css');
+
+    this.mkdir('static/pages/about');
+    this.copy('about/info.json', 'static/pages/about/info.json');
+    this.copy('about/content.md', 'static/pages/about/content.md');
+};
---
+++ app/templates/_server.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+	Paperpress = require('paperpress').Paperpress;
+
+var server = express();
+
+var blog = new Paperpress({
+	directory : 'static',
+	basePath  : '/blog',
+	pagesPath : ''
+});
+
+blog.attach(server);
+
+server.get('/', function (req, res) {
+	res.redirect('/blog');
+});
+
+server.listen(3000);
+console.log('Server running at http://localhost:3000');
